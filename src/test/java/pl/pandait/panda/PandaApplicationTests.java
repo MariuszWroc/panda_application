23b670616ac7fecd484960f7664292b24464a740
Mariusz <developeronthego@yandex.com>
2021-05-14 17:56:01 +0000

    Mariusz changes

--- src/test/java/pl/pandait/panda/PandaApplicationTests.java
+++ src/test/java/pl/pandait/panda/PandaApplicationTests.java
@@ -3,11 +3,19 @@ package pl.pandait.panda;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PandaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest{
+    MockMvc mockMvc;
+    @BeforeEach
+    public void beforeSetUp(){
+        this.mockMvc = standaloneSetup(new HelloController()).build();
+    }
+    @Test
+    public void whenGivenAttributeNameThenItIsInModel() throws Exception{
+    this.mockMvc.perform(get("/greeting?name=Panda")
+	.accept(MediaType.ALL))
+	.andExpect(status().isOk())
+	.andExpect(model().attribute("name", "Panda"));
+    }
 }
+
