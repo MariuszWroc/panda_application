7d5e9ab556bd33b17f80e71d3ed73f527ba35e7f
Mariusz <developeronthego@yandex.com>
2021-05-17 17:04:40 +0000

    Selenium grid

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -31,4 +31,28 @@ public class PandaApplicationSeleniumTest {
         driver = new RemoteWebDriver(new URL("http://192.168.44.44:4444/wd/hub"), capabilities);
         driver.get(String.format("http://192.168.44.44:%d", port));
     }
+
+    @Test
+    public void test1() {
+        System.out.println("Uruchamiam test 1: Sprawdzenie napisu na stronie głównej");
+        WebElement greetingElement = driver.findElement(By.xpath("//p"));
+        String greetingText = greetingElement.getText().trim();
+        assertEquals("Get your greeting here", greetingText);
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Uruchamiam test 2: Sprawdzenie napisu na podstronie");
+        WebElement greetingElement = driver.findElement(By.xpath("//p"));
+        WebElement linkToGreetings = greetingElement.findElement(By.xpath("./a"));
+        linkToGreetings.click();
+        WebElement helloWorldString = driver.findElement(By.xpath("//p"));
+        String newPageString = helloWorldString.getText().trim();
+        assertEquals("Hello, World!", newPageString);
+    }
+
+    @AfterEach
+    public void after() {
+        driver.quit();
+    }
 }
